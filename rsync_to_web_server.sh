b2a2378ee51eb8811af6e831a3e74e1c28a37552
Massimo Zaniboni <massimo.zaniboni@asterisell.com>
2016-05-12 19:16:31 +0200

    Updated rsync with stack support.

--- rsync_to_web_server.sh
+++ rsync_to_web_server.sh
@@ -1,4 +1,4 @@
-./dist/build/site/site clean
-./dist/build/site/site build
+.stack-work/install/x86_64-linux/lts-5.14/7.10.3/bin/site clean
+.stack-work/install/x86_64-linux/lts-5.14/7.10.3/bin/site build
 rsync --chown=www-data:www-data --cvs-exclude -avzhe ssh ./_site/ root@asterisell.com:/var/www/www_haskell-ita_it
 
