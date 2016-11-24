1f4e20e91d2542a3e0f36dad7f51748994840c4d
Massimo Zaniboni <massimo.zaniboni@docmelody.com>
2016-11-24 19:23:53 +0100

    Upgraded to new Hakyll and Stackage LTS

--- site.hs
+++ site.hs
@@ -23,7 +23,7 @@ import qualified  Data.Map                         as M
 import qualified  Text.Blaze.Html5                 as H
 import qualified  Text.Blaze.Html5.Attributes      as A
 import            System.FilePath
-import            Hakyll
+import            Hakyll as Hakyll
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -173,7 +173,7 @@ feedsCtx =
 getPeople :: MonadMetadata m => Identifier -> m [String]
 getPeople identifier = do
   metadata <- getMetadata identifier
-  return $ maybe [] (map trim . splitAll ",") $ M.lookup "author" metadata
+  return $ maybe [] (map trim . splitAll ",") $ Hakyll.lookupString "author" metadata
 
 --------------------------------------------------------------------------------
 -- ROUTES
@@ -313,7 +313,7 @@ tryParseDate = tryParseDateWithLocale defaultTimeLocale
 
 tryParseDateWithLocale :: TimeLocale -> Identifier -> Metadata -> Maybe UTCTime
 tryParseDateWithLocale locale id' metadata = do
-   let tryField k fmt = M.lookup k metadata >>= parseTime' fmt
+   let tryField k fmt = Hakyll.lookupString k metadata >>= parseTime' fmt
        fn             = takeFileName $ toFilePath id'
 
    maybe empty' return $ msum $
